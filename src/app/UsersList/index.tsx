c3f744902540ee51afd5f78e5c5a599a1930349d
Ojasvita <ojasvita2308@gmail.com>
2025-04-17 00:33:57 +0530

    search bar

--- src/app/UsersList/index.tsx
+++ src/app/UsersList/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import UserThread from "../UserInfo";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,33 +10,52 @@ import { fetchUsers } from "@/lib/features/users/userListCounter";
 
 const UsersList = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { users, loading, error } = useSelector((state: RootState) => state.users);
+  const { users, loading, error } = useSelector(
+    (state: RootState) => state.users
+  );
   const count = useSelector((state: RootState) => state.counter.value);
 
+  const [searchValue, setSearchValue] = useState("");
+
   useEffect(() => {
-    dispatch(fetchUsers())
+    dispatch(fetchUsers());
   }, [dispatch]);
+
+  const filteredUsers = users.filter((user) =>
+    `${user.firstName} ${user.lastName}`
+      .toLowerCase()
+      .includes(searchValue.toLowerCase())
+  );
 
   return (
     <div>
       <div>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
       </div>
-    </div>
-      {users.map((item) => {
+      <div>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="mb-4 p-2 w-full"
+        />
+      </div>
+      {filteredUsers.map((item) => {
         return <UserThread key={item.id} userInfo={item} />;
       })}
     </div>
